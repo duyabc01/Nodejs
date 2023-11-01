dfe64da1895116adfd7d8ec6a4d1dcf4faaa3d99
duyabc01 <phamminhduyxbxtnd@gmail.com>
2023-11-01 15:41:16 +0700

    profile

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('node:http');
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>aaaaaaaaaaaaaaaaaaaaa</h1>
+
+</body>
+</html>
+    `
+  );
+});
+
+server.listen(8000, () =>{
+    console.log("sever running on port 8000")
+});
